2be0a68f92e50644cb5092aa137e3a433a624132
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-30 16:13:58 +1000

    Communication ght ites prop

--- sg-react-136-widgets/src/App.js
+++ sg-react-136-widgets/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./App.css";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Widgets App</h1>
+      <Accordion items={items} />
     </div>
   );
 }
---
+++ sg-react-136-widgets/src/components/Accordion.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Accordion = ({ items }) => {
+  return (
+    <div>
+      <h1>{items.length}</h1>
+    </div>
+  );
+};
+export default Accordion;
