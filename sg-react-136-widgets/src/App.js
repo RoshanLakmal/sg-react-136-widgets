404508ddb188f19acd04ba0d1e8cca0dca6960e5
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-30 16:28:36 +1000

    building and styling the according

--- sg-react-136-widgets/src/App.js
+++ sg-react-136-widgets/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import Accordion from "./components/Accordion";
 
 const items = [
--- sg-react-136-widgets/src/components/Accordion.js
+++ sg-react-136-widgets/src/components/Accordion.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
+  const renderItems = items.map((item) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon">{item.title}</i>
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
   return (
-    <div>
-      <h1>{items.length}</h1>
+    <div className="ui styled accordion">
+      <h1>{renderItems}</h1>
     </div>
   );
 };
