b5b05ffe21c844f9f6ed1dfd47efb80619b3bd6e
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-30 19:52:41 +1000

    added dropdown

--- sg-react-136-widgets/src/components/Search.js
+++ sg-react-136-widgets/src/components/Search.js
@@ -2,25 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   // console.log(results);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
-    /* approach -1  - React */
-    // const search = async() => {
-    //     await axios.get('abc');
-    // };
-    // search();
-    /* approach -1  */
-    // (async() => {
-    //     await axios.get('abc');
-    // })();
-    /* approach -3  - promise */
-    // axios.get('abc')
-    //   .then((response) => {
-    //     console.log(response.data);
-    //   });
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
@@ -28,30 +24,61 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
+
       setResults(data.query.search);
     };
+    search();
+  }, [debouncedTerm]);
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
+  // useEffect(() => {
+  /* approach -1  - React */
+  // const search = async() => {
+  //     await axios.get('abc');
+  // };
+  // search();
+  /* approach -1  */
+  // (async() => {
+  //     await axios.get('abc');
+  // })();
+  /* approach -3  - promise */
+  // axios.get('abc')
+  //   .then((response) => {
+  //     console.log(response.data);
+  //   });
+  // const search = async () => {
+  //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+  //     params: {
+  //       action: "query",
+  //       list: "search",
+  //       origin: "*",
+  //       format: "json",
+  //       srsearch: term,
+  //     },
+  //   });
+  //   setResults(data.query.search);
+  // };
 
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-    // console.log("Initial render or term was changed");
-    // return () => {
-    //   console.log("CLEANUP");
-    // };
-  }, [term]);
+  // if (term && !results.length) {
+  //   search();
+  // } else {
+  //   const timeoutId = setTimeout(() => {
+  //     if (term) {
+  //       search();
+  //     }
+  //   }, 1000);
+
+  //   return () => {
+  //     clearTimeout(timeoutId);
+  //   };
+  // }
+  // console.log("Initial render or term was changed");
+  // return () => {
+  //   console.log("CLEANUP");
+  // };
+  // }, [term, results.length]);
 
   //   useEffect(() => {
   //     console.log("when ever render");
