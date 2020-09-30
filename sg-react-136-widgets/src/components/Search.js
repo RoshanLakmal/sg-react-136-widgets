c1a9422f2bd8fc1233463f6d70abe3427da7d66d
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-30 17:48:27 +1000

    Executing the Request from useEffect

--- sg-react-136-widgets/src/components/Search.js
+++ sg-react-136-widgets/src/components/Search.js
@@ -1,12 +1,39 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  console.log("I RUN WITH EVERY RENDER");
   useEffect(() => {
-    console.log("I RUN AFTER EVERY RENDER AND AT INITIAL RENDER");
-  }, [term]);
+    /* approach -1  - React */
+    // const search = async() => {
+    //     await axios.get('abc');
+    // };
+    // search();
+
+    /* approach -1  */
+    // (async() => {
+    //     await axios.get('abc');
+    // })();
+
+    /* approach -3  - promise */
+    // axios.get('abc')
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   });
+
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+    search();
+  });
 
   //   useEffect(() => {
   //     console.log("when ever render");
