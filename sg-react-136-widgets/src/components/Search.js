8b168df2aec6c6a4fe037493bd76adac65e95c1f
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-30 18:35:24 +1000

    added link to wikipedia page

--- sg-react-136-widgets/src/components/Search.js
+++ sg-react-136-widgets/src/components/Search.js
@@ -48,9 +48,18 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          {/* {result.snippet} */}
         </div>
       </div>
     );
